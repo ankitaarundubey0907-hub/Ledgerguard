ed4e66ee1c263c5b814394ee4d30f0051235854e
sanjana pal <australia789abc@gmail.com>
2026-08-28 20:08:02 +0530

    Setup backend with Express

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+const express=require("express")
+const dotenv=require("dotenv")
+dotenv.config()
+
+const app=express()
+app.use(express.json())
+app.get("/health",(req,res)=>{
+   res.json({
+        success: true,
+        message: "LedgerGuard backend is running"
+    });  
+})
+const port= process.env.port || 5000;
+
+app.listen(port,()=>{
+    console.log(`LedgerGuard server running on port ${port}`);
+});
