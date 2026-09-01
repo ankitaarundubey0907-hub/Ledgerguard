7be1db14e65f9758f5ec5e83af84d0ff2be47c1c
sanjana pal <australia789abc@gmail.com>
2026-09-01 20:53:50 +0530

    mongoose connected

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,14 @@
 const express=require("express")
 const dotenv=require("dotenv")
+const connectDB = require("./config/db");
 dotenv.config()
+
 
 const app=express()
 app.use(express.json())
+
+connectDB();
+
 app.get("/health",(req,res)=>{
    res.json({
         success: true,
